e6990ca12363b154e2b16dcee08961641c65b776
unknown <altaai.ahmed@gmx.at>
2020-11-09 22:23:00 +0100

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
   parserOptions: {
     parser: 'babel-eslint'
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  extends: [
+    'akarion/vue',
+  ],
+};
